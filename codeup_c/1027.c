277b5e459a613e853c4970f51d0788274a9cf870
paulham98 <pauljungho.ham@gmail.com>
2021-03-05 17:54:08 +0900

    2021_03_05

---
+++ codeup_c/1027.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    int y,m,d;
+    scanf("%d.%d.%d", &y, &d, &m);
+    printf("%d-%d-%d", d, m, y);
+}
---
+++ codeup_c/1047.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int a = 10;
+    printf("%d\n", a<<2);
+    printf("%d\n", a>>1);
+    printf("%d", a>>2);
+
+}
