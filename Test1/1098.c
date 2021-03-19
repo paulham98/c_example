4b9c98bb554fe8ca4ea6fe6ba4bcee82a0118146
paulham98 <pauljungho.ham@gmail.com>
2021-03-19 21:38:47 +0900

    2021_3_19

--- Test1/1098.c
+++ Test1/1098.c
@@ -5,23 +5,22 @@ int main(){
     scanf("%d", &a);
     int b= 0;
     // int** box = (int**)malloc(sizeof(int*) * a);
-    int pi[5][5];
+    int pi[5][5]= {0,};
     while(b < a){
         int q,w,e,r = 0;
         scanf("%d %d %d %d", &q, &w, &e, &r);
-        e--;
-        r--;
         if( w == 0){//가로
-            for(int i = e; i < q; i++){
-            pi[i][r] = 1;
+            for(int i = r-1; i < q; i++){
+            pi[e-1][i] = 1;
             }
         }else{//세로 
-            for(int i = r; i < q; i++){
-                pi[e][i] = 1;
+            for(int i = e-1; i <= q; i++){
+                pi[i][r-1] = 1;
             }
         }
         b++;
     }
+
     for(int i = 0; i < 5; i++){
         for(int j=0; j< 5; j++){
             printf("%d ", pi[i][j]);
