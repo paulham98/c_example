82f48465ea25328a2835426d96cee150b88bb181
paulham98 <pauljungho.ham@gmail.com>
2021-03-25 22:58:05 +0900

    Rectangle

---
+++ Test1/Rectangle.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include "Rectangle.h"
+
+int main() {
+	
+}
---
+++ Test1/Rectangle.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+struct Rectangle{
+private:
+	int xLow, yLow, height, width;
+
+public:
+	inline int getHeight(int xLow, int yLow) {
+		this->xLow = xLow;
+		this->yLow = yLow;
+		int h = xLow * yLow;
+		printf("≥Ù¿Ã: %d", &h);
+	}
+	inline int GetWidth() {
+
+	}
+};
