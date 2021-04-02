615e017a31ca8894b3a6e27a8e3f7e75882e8be5
paulham98 <pauljungho.ham@gmail.com>
2021-04-02 19:09:47 +0900

    2021_04_02

--- Test1/C_MagicSquare.cpp
+++ Test1/C_MagicSquare.cpp
@@ -11,15 +11,22 @@ int main(){
         for(int j = 0; j < 5; j++){
             ++cnt;
             --a,--b;
-            if(a < 0) a = 4;
-            if(b < 0) b = 4;
+            if(a < 0) a += 5;
+            if(b < 0) b += 5;
+
             if(ms[a][b] != 0) {
-                --b;
-                a += 2;
-                if(a < 0 || a > 5) a = 4;
-                if(b < 0) b =4;
+                a++;
+                b++;
+                if(a > 4) a -= 5;
+                if(b > 4) b -= 5;
+                if(ms[a][b] == cnt-1) {
+                    a++; 
+                    ms[a][b] = cnt;
+                    continue;
+                }
+                
             }
-            
+
             ms[a][b] = cnt;
              
         }
