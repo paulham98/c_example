198dca78621de6f0364a3f2ac331581c592c79e1
paulham98 <pauljungho.ham@gmail.com>
2021-05-08 21:49:32 +0900

    2021_05_08

--- Test1/Hw.Hanoi.cpp
+++ Test1/Hw.Hanoi.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 void Hanoi(int n, char from, char by, char to) {
 	/*	
-		int n : �̵��� ������ ����
-		char from : �̵��� ������ �ִ� ����� ž
-		char by : ������ �̵��ϴ� �������� ���İ��� ž
-		char to : ������ �̵��� ������ ž
+		int n : 이동할 원반의 숫자
+		char from : 이동할 원반이 있는 출발지 탑
+		char by : 원반을 이동하는 과정에서 거쳐가는 탑
+		char to : 원반을 이동할 목적지 탑
 	*/
 	if(n == 1){
 		cout << from << " => " << to << endl;
