40cd4dc187d39fee3373d7911e0c597bbf74c35a
paulham98 <pauljungho.ham@gmail.com>
2021-05-14 20:03:33 +0900

    2021_05_14

--- Test1/C_Queue.cpp
+++ Test1/C_Queue.cpp
@@ -1,28 +1,73 @@
 #include<iostream>
-
 using namespace std;
-template <typename T>
-class Queue {
-public:
-	void Enqueue(const T&);
-	void Dequeue();
-	T& Front();
-	boolean IsEmpty();
+
+template <class T>class Queue;
+
+template <class T>class Node{
+	friend class Queue<T>;
 private:
-	int capacity;
-	int front = -1;
-	int rear;
+	T data;
+	Node<T>* next;
+	Node<T>* prev;
 };
-class PriorityQ {
-	friend Queue;
+
+template <class T>class Queue {
+private:
+	Node<T>* front;
+	Node<T>* rear;
+	int size = 0;
+public:
+	void Enqueue(T data){
+		Node<T>* new_node = new Node<T>;
+		size++;
+		new_node->data = data;
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		if(front == NULL){
+			front = new_node;
+			rear = new_node;
+		}else {
+			new_node->next = front;
+			front->prev = new_node;
+			front = new_node;
+		}
+	};
+	void Dequeue(){
+		if(front == NULL){
+			printf("Queue is Empty!");
+		}else{
+		cout << rear->data << endl;
+		rear = rear->prev;
+		rear->next = NULL;
+		}
+	};
+	void Front(){
+		cout << front->data << endl;
+	};
+	void Length(){
+		return size;
+	}
+	void Print(){
+		Node<T> *cur = NULL;
+		for(cur = front; cur != NULL; cur = cur->next){
+			cout << cur->data << " ";
+		}
+		printf("\n");
+	}
 };
+
 
 int main() {
+	Queue<int> q;
+
+	int number;
+	q.Enqueue(1);
+	q.Enqueue(2);
+	q.Enqueue(3);
+	q.Enqueue(4);
+	q.Print();
+	q.Dequeue();
+	q.Print();
 
 
 }
-void Enqueue(const T&) {
-};
-void Dequeue();
-T& Front();
-boolean IsEmpty();
---
+++ Test1/PriorityQ.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+#define MAX 200;
+
+template <class T>class TreeNode{
+private:
+    int key;
+    T data;
+public:
+    int getKey();
+    void setKey(int key);
+    T getData();
+};
+template <class T>class MaxHeap{
+private:
+    TreeNode<T> node[MAx];
+    int size;
+public:
+    
+};
+
+int main(){
+
+}
+
