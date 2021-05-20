5c7445d302286ec4d5318fbdd833354b17ba9393
paulham98 <pauljungho.ham@gmail.com>
2021-05-21 02:08:20 +0900

    2021)05_21

--- Test1/graph.cpp
+++ Test1/graph.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<fstream>
 #include<string>
 #include<stdio.h>
 #include "graph.h"
@@ -7,26 +8,75 @@ using namespace std;
 // memory			   time complexity
 
 void Graph:: LoadMatrix(std::string& filename){
-    cin >> n;
+    /*cin >> n;
     matrix = new int* [n];
     for (int i = 0; i < n; i++) {
         matrix[i] = new int[n];
     }
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            scanf("%p ", &vertex);
-            vertex = *(matrix+j);
+            scanf("%d ", &vertex);
+            matrix[i][j] = vertex;
         }
     }
+    */
+    ifstream fin(filename);
+    fin >> n;
+    cout << n << endl;
+   
+    
+    char ch = NULL;
+    while(fin.get(ch)){
+        fin >> vertex;
+        cout << vertex;
+    }
+   
+    if (!fin.is_open()) {
+        cout << "error" << endl;
+    }
+    cout << " 열림 " << endl;
+    /*
+     for (int i = 0; i < n; i++) {
+        matrix[i] = new int[n];
+    } // 2차원 배열 생성;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            fin >> vertex;
+            cout << vertex;
+            matrix[i][j] = vertex;
+        }
+    }
+    
+    while (fin.get(ch)) {
+        fin >> vertex;
+        cout << vertex;
+    }
+    */
+        
+    fin.close();
 };
 void Graph:: PrintMatrix(){
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            printf("%p", *(matrix+j));
+            cout << matrix[i][j] << " ";
         }
-        printf("\n");
+        cout << "\n";
     }
-    for (int i = 0; i < n; i++) {
-        delete matrix[i];
-    }
+    
 };
+
+
+int main(void) {
+    Graph g;
+
+    string filename;
+    getline(cin, filename);
+
+    g.LoadMatrix(filename);
+    g.PrintMatrix();
+
+    return 0;
+}
+
+
--- Test1/graph.h
+++ Test1/graph.h
@@ -3,12 +3,20 @@
 #include<string>
 using namespace std;
 
+
 class Graph{
 private:
     int n;
-    int *vertex;
-    int** matrix;
+    int vertex;
+    int** matrix = nullptr;
 public:
+
+    ~Graph() {
+        for (int i = 0; i < n; i++) {
+            delete[] matrix[i];
+        }
+        delete[] matrix;
+    }
     void LoadMatrix(std::string& filename);
     void PrintMatrix();
 };
