2e084fa067be284ac49bfdc9ca518bbcbe4b37c6
paulham98 <pauljungho.ham@gmail.com>
2021-06-18 17:48:06 +0900

    2021_06_18

--- Test1/avlTree.cpp
+++ Test1/avlTree.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
 #include<vector>
+using namespace std;
+template<class T> class Node {
+private:
+	T data;
+	Node<T>* LeftChild;
+	Node<T>* RightChild;
+};
 class Bst {
 private:
 	int* node;
 	int element;
 	int size;
+	int n;
 public:
 	int GetSize() {
 		return size;
